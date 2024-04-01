ff740370fee6d801fcdef36909869af7b0b8cc81
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-04-01 15:06:54 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationCRUD_USERS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
